9457f540e8e4adcc1c405f16e19a37ba33871c24
Matt Braddock <matt.braddock@gmail.com>
2025-12-02 09:53:05 -0500

    day 2

---
+++ day02.js
@@ -0,0 +1,28 @@
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+const puzzleInput = readFileSync(fileURLToPath(import.meta.url).replace('js', 'txt'), 'utf8').split(',');
+
+const example = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`.split(',');
+
+const main = array => {
+    const invalidIdsPart1 = [];
+    const invalidIdsPart2 = [];
+    array.forEach(range => {
+        const [ start, end ] = range.split('-');
+        for (let i = Number(start); i <= Number(end); i++) {
+            if (/^(\d+)\1$/.test(i.toString())) {
+                invalidIdsPart1.push(i);
+            }
+            if (/^(\d+)\1+$/.test(i.toString())) {
+                invalidIdsPart2.push(i);
+            }
+        }
+    });
+    return [ invalidIdsPart1.reduce((sum, val) => sum + val, 0), invalidIdsPart2.reduce((sum, val) => sum + val, 0) ];
+}
+
+console.log(`expected: 1227775554, calculated: ${main(example)[0]}`);
+console.log(`part one solution: ${main(puzzleInput)[0]}`);
+console.log(`expected: 4174379265, calculated: ${main(example)[1]}`);
+console.log(`part two solution: ${main(puzzleInput)[1]}`);
